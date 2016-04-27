3c66d8eb32d070acdd12af32b0ae03533424266e
INTI_0269 <INTI_0269@INTI0269>
2016-04-27 17:40:57 +0200

    ok

--- ProjetGroupe4Tp1/src/main/java/com/gestiondecompte/gestiondecompte/Controleur/Controleur.java
+++ ProjetGroupe4Tp1/src/main/java/com/gestiondecompte/gestiondecompte/Controleur/Controleur.java
@@ -1,5 +1,19 @@
 package com.gestiondecompte.gestiondecompte.Controleur;
 
-public class Controleur {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-}
+import com.gestiondecompte.gestiondecompte.Metier.ImplMetier;
+
+@Controller
+public class Controleur {
+	@Autowired
+	private ImplMetier metier;
+
+	@RequestMapping(value = "/index")
+	public String index() {
+		return "banqueaghiles";
+	}
+} 
+ 
